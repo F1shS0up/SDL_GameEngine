b39e8f02343f81a173fd29310ae348cc53f95a04
F1shS0up <alkanjebest@gmail.com>
2023-11-24 15:38:30 +0100

    More flexible tilemap
    with c_ as collision layer and t_ as tile layer

--- SDL_GameEngine/Tilemap.cpp
+++ SDL_GameEngine/Tilemap.cpp
@@ -40,167 +40,152 @@ namespace Engine
 					reg->tilemaps[e].grid[y] = (std::pair<int, int>*)malloc(reg->tilemaps[e].gridCellsX * sizeof(std::pair<int, int>));
 					for (int x = 0; x < reg->tilemaps[e].gridCellsX; x++)
 					{
-						if (tilemap["layers"][0]["dataCoords2D"][y][x].size() < 2)
+						for(int i = 0; i < tilemap["layers"].size(); i++)
 						{
-							reg->tilemaps[e].grid[y][x].first = -1;
-							reg->tilemaps[e].grid[y][x].second = -1;
-						}
-						else
-						{
-							reg->tilemaps[e].grid[y][x].first = tilemap["layers"][0]["dataCoords2D"][y][x][0].asInt();
-							reg->tilemaps[e].grid[y][x].second = tilemap["layers"][0]["dataCoords2D"][y][x][1].asInt();
-						}
-						if (tilemap["layers"][1]["grid2D"][y][x] == "1" && colCheck[y][x] == 0)
-						{
-							Vector2DInt dir = Vector2DInt(1, 0);
-							Vector2D A = Vector2D(x * reg->tilemaps[e].gridCellW,
-								y * reg->tilemaps[e].gridCellH);
-							Vector2D start = A;
-
-							Vector2D B = Vector2D(x * reg->tilemaps[e].gridCellW + reg->tilemaps[e].gridCellW,
-								y * reg->tilemaps[e].gridCellH);
-
-							std::cout << "Pos: " << A.x << " " << A.y << std::endl;
-
-							int newX = x;
-							int newY = y;
-
-							while (true)
+							if (tilemap["layers"][i]["name"].asCString()[0] == 't' && tilemap["layers"][i]["name"].asCString()[1] == '_')
 							{
-								int increment = 1;
-								std::cout << "UPDATE" << std::endl;
-								while (tilemap["layers"][1]["grid2D"][newY + dir.y * increment][newX + dir.x * increment] == "1")
+								if (tilemap["layers"][i]["dataCoords2D"][y][x].size() < 2)
 								{
-									colCheck[newY + dir.y * increment][newX + dir.x * increment] = 1;
+									reg->tilemaps[e].grid[y][x].first = -1;
+									reg->tilemaps[e].grid[y][x].second = -1;
+								}
+								else
+								{
+									reg->tilemaps[e].grid[y][x].first = tilemap["layers"][i]["dataCoords2D"][y][x][0].asInt();
+									reg->tilemaps[e].grid[y][x].second = tilemap["layers"][i]["dataCoords2D"][y][x][1].asInt();
+								}
+							}
+							else if (tilemap["layers"][i]["name"].asCString()[0] == 'c' && tilemap["layers"][i]["name"].asCString()[1] == '_')
+							{
+								if (tilemap["layers"][i]["grid2D"][y][x] == "1" && colCheck[y][x] == 0)
+								{
+									Vector2DInt dir = Vector2DInt(1, 0);
+									Vector2D A = Vector2D(x * reg->tilemaps[e].gridCellW,
+										y * reg->tilemaps[e].gridCellH);
+									Vector2D start = A;
 
-									std::cout << "Dir check" << std::endl;
-									std::cout << newY + dir.y * increment << "    " << newX + dir.x * increment << std::endl;
-									std::cout << newY << "    " << newX << std::endl;
-									std::cout << dir.y << "    " << dir.x << std::endl;
-									std::cout << increment << std::endl;
+									Vector2D B = Vector2D(x * reg->tilemaps[e].gridCellW + reg->tilemaps[e].gridCellW,
+										y * reg->tilemaps[e].gridCellH);
 
-									if (dir.x > 0)
+									std::cout << "Pos: " << A.x << " " << A.y << std::endl;
+
+									int newX = int(x);
+									int newY = int(y);
+
+									while (true)
 									{
-										B = Vector2D((newX + dir.x * increment) * reg->tilemaps[e].gridCellW + reg->tilemaps[e].gridCellW,
-											(newY + dir.y * increment) * reg->tilemaps[e].gridCellH);
+										int increment = 1;
+
+										std::cout << "X:" << newX + dir.x * increment << "Y:" << newY + dir.y * increment << std::endl;
+
+										while (newX + dir.x * increment >= 0 && newX + dir.x * increment < reg->tilemaps[e].gridCellsX && newY + dir.y * increment >= 0 && newY + dir.y * increment < reg->tilemaps[e].gridCellsY && tilemap["layers"][i]["grid2D"][newY + dir.y * increment][newX + dir.x * increment] == "1")
+										{
+											std::cout << "UWU" << std::endl;
+											colCheck[newY + dir.y * increment][newX + dir.x * increment] = 1;
+
+											if (dir.x > 0)
+											{
+												B = Vector2D((newX + dir.x * increment) * reg->tilemaps[e].gridCellW + reg->tilemaps[e].gridCellW,
+													(newY + dir.y * increment) * reg->tilemaps[e].gridCellH);
+											}
+											else if (dir.x < 0)
+											{
+												B = Vector2D((newX + dir.x * increment) * reg->tilemaps[e].gridCellW,
+													(newY + dir.y * increment) * reg->tilemaps[e].gridCellH + reg->tilemaps[e].gridCellH);
+											}
+											else if (dir.y > 0)
+											{
+												B = Vector2D((newX + dir.x * increment) * reg->tilemaps[e].gridCellW + reg->tilemaps[e].gridCellW,
+													(newY + dir.y * increment) * reg->tilemaps[e].gridCellH + reg->tilemaps[e].gridCellH);
+											}
+											else if (dir.y < 0)
+											{
+												B = Vector2D((newX + dir.x * increment) * reg->tilemaps[e].gridCellW,
+													(newY + dir.y * increment) * reg->tilemaps[e].gridCellH);
+											}
+
+											increment++;
+										}
+
+										Entity l = EntityManager::Instance()->CreateEntity();
+										reg->lineColliders[l] = LineCollider_Component{ A, B };
+										A = B;
+
+										if (Vector2D(B.x, B.y) == start) break;
+
+										newX = newX + dir.x * (increment - 1);
+										newY = newY + dir.y * (increment - 1);
+										std::cout << "X:" << newX << "Y:" << newY << std::endl;
+										if (newY > 0 && tilemap["layers"][i]["grid2D"][newY - 1][newX] == "1" && dir.y == 0)
+										{
+											// top
+											std::cout << "Top" << std::endl;
+											dir = Vector2DInt(0, -1);
+
+
+										}
+										else if (newY < reg->tilemaps[e].gridCellsY - 1 && tilemap["layers"][i]["grid2D"][newY + 1][newX] == "1" && dir.y == 0)
+										{
+											// down
+											std::cout << "Down" << std::endl;
+											dir = Vector2DInt(0, 1);
+
+										}
+										else if (newX > 0 && tilemap["layers"][i]["grid2D"][newY][newX - 1] == "1" && dir.x == 0)
+										{
+											// leftB
+											std::cout << "Left" << std::endl;
+											dir = Vector2DInt(-1, 0);
+
+										}
+										else if (newX < reg->tilemaps[e].gridCellsX - 1 && tilemap["layers"][i]["grid2D"][newY][newX + 1] == "1" && dir.x == 0)
+										{
+											// right
+											std::cout << "Right" << std::endl;
+											dir = Vector2DInt(1, 0);
+
+										}
+										else if (dir.x < 0)
+										{
+											// top
+											std::cout << "Top" << std::endl;
+											dir = Vector2DInt(0, -1);
+											B = A + Vector2D(0, -reg->tilemaps[e].gridCellH);
+										}
+										else if (dir.x > 0)
+										{
+											// down
+											std::cout << "Down" << std::endl;
+											dir = Vector2DInt(0, 1);
+											B = A + Vector2D(0, reg->tilemaps[e].gridCellH);
+										}
+										else if (dir.y < 0)
+										{
+											// left
+											std::cout << "Left" << std::endl;
+											dir = Vector2DInt(-1, 0);
+											B = A + Vector2D(-reg->tilemaps[e].gridCellW, 0);
+										}
+										else if (dir.y > 0)
+										{
+											// right
+											std::cout << "Right" << std::endl;
+											dir = Vector2DInt(1, 0);
+											B = A + Vector2D(reg->tilemaps[e].gridCellW, 0);
+
+										}
+
 									}
-									else if (dir.x < 0)
-									{
-										B = Vector2D((newX + dir.x * increment) * reg->tilemaps[e].gridCellW,
-											(newY + dir.y * increment) * reg->tilemaps[e].gridCellH + reg->tilemaps[e].gridCellH);
-									}
-									else if (dir.y > 0)
-									{
-										B = Vector2D((newX + dir.x * increment) * reg->tilemaps[e].gridCellW + reg->tilemaps[e].gridCellW,
-											(newY + dir.y * increment) * reg->tilemaps[e].gridCellH + reg->tilemaps[e].gridCellH);
-									}
-									else if (dir.y < 0)
-									{
-										B = Vector2D((newX + dir.x * increment) * reg->tilemaps[e].gridCellW,
-											(newY + dir.y * increment) * reg->tilemaps[e].gridCellH);
-									}
-									std::cout << "Dir check End" << std::endl;
-									
-									increment++;
-
 								}
-
-								Entity l = EntityManager::Instance()->CreateEntity();
-								reg->lineColliders[l] = LineCollider_Component{ A, B };
-								A = B;
-
-								if (Vector2D(B.x, B.y) == start) break;
-
-								std::cout << "Pos: " << B.x << " " << B.y << std::endl;
-
-								newX = newX + dir.x * (increment - 1);
-								newY = newY + dir.y * (increment - 1);
-								if (newY != 0 && tilemap["layers"][1]["grid2D"][newY - 1][newX] == "1" && dir.y == 0)
-								{
-									// top
-									std::cout << "Top" << std::endl;
-									dir = Vector2DInt(0, -1);
-									
-									
-								}
-								else if (newY != reg->tilemaps[e].gridCellsY - 1 && tilemap["layers"][1]["grid2D"][newY + 1][newX] == "1" && dir.y == 0)
-								{
-									// down
-									std::cout << "Down" << std::endl;
-									dir = Vector2DInt(0, 1);
-
-								}
-								else if (newX != 0 && tilemap["layers"][1]["grid2D"][newY][newX - 1] == "1" && dir.x == 0)
-								{
-									// leftB
-									std::cout << "Left" << std::endl;
-									dir = Vector2DInt(-1, 0);
-
-								}
-								else if (newX != reg->tilemaps[e].gridCellsX - 1 && tilemap["layers"][1]["grid2D"][newY][newX + 1] == "1" && dir.x == 0)
-								{
-									// right
-									std::cout << "Right" << std::endl;
-									dir = Vector2DInt(1, 0);
-
-								}
-
-
-								std::cout << "Dir update" << std::endl;
-
 							}
 
-
-							////TODO Create more optimized way by linking line collider next to each other...
-							//if (y == 0 || tilemap["layers"][1]["grid2D"][y - 1][x] == "0")
-							//{
-							//	// top
-							//	Vector2D A = Vector2D(x * reg->tilemaps[e].gridCellW, 
-							//		y * reg->tilemaps[e].gridCellH);
-							//	Vector2D B = Vector2D(x * reg->tilemaps[e].gridCellW + reg->tilemaps[e].gridCellW, 
-							//		y * reg->tilemaps[e].gridCellH);
-
-							//	Entity l = EntityManager::Instance()->CreateEntity();
-							//	reg->lineColliders[l] = LineCollider_Component{A, B};
-							//}
-							//if (y == reg->tilemaps[e].gridCellsY - 1 || tilemap["layers"][1]["grid2D"][y + 1][x] == "0")
-							//{
-							//	// down
-							//	Vector2D A = Vector2D(x * reg->tilemaps[e].gridCellW, 
-							//		y * reg->tilemaps[e].gridCellH + reg->tilemaps[e].gridCellH);
-							//	Vector2D B = Vector2D(x * reg->tilemaps[e].gridCellW + reg->tilemaps[e].gridCellW, 
-							//		y * reg->tilemaps[e].gridCellH + reg->tilemaps[e].gridCellH);
-
-							//	Entity l = EntityManager::Instance()->CreateEntity();
-							//	reg->lineColliders[l] = LineCollider_Component{ A, B };
-							//}
-
-							//if (x == 0 || tilemap["layers"][1]["grid2D"][y][x - 1] == "0")
-							//{
-							//	// left
-							//	Vector2D A = Vector2D(x * reg->tilemaps[e].gridCellW,
-							//		y * reg->tilemaps[e].gridCellH);
-							//	Vector2D B = Vector2D(x * reg->tilemaps[e].gridCellW,
-							//		y * reg->tilemaps[e].gridCellH + reg->tilemaps[e].gridCellH);
-
-							//	Entity l = EntityManager::Instance()->CreateEntity();
-							//	reg->lineColliders[l] = LineCollider_Component{ A, B };
-							//}
-							//if (x == reg->tilemaps[e].gridCellsX - 1 || tilemap["layers"][1]["grid2D"][y][x + 1] == "0")
-							//{
-							//	// right
-							//	Vector2D A = Vector2D(x * reg->tilemaps[e].gridCellW + reg->tilemaps[e].gridCellW,
-							//		y * reg->tilemaps[e].gridCellH);
-							//	Vector2D B = Vector2D(x * reg->tilemaps[e].gridCellW + reg->tilemaps[e].gridCellW,
-							//		y * reg->tilemaps[e].gridCellH + reg->tilemaps[e].gridCellH);
-
-							//	Entity l = EntityManager::Instance()->CreateEntity();
-							//	reg->lineColliders[l] = LineCollider_Component{ A, B };
-							//}
+							
 						}
 						
 					}
 				}
+
+				std::cout << reg->lineColliders.size() << std::endl;
 
 				reg->tilemaps[e].tileset.texture = IMG_LoadTexture(renderer, reg->tilemaps[e].tileset.tilesetPath.c_str());
 			}
